dba8605b80d714689bff5a513c9e8caf7ba88d05
Darth038 <darkvignesh2002@gmail.com>
2025-09-20 22:12:18 +0530

    Completed all functions in script.js

--- script.js
+++ script.js
@@ -8,20 +8,40 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((emp) => {
+    if (emp.profession == "developer") {
+      console.log(emp);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((emp) => {
+    if (emp.profession == "developer") {
+      console.log(emp);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({ id: 4, name: "susan", age: "20", profession: "intern" });
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((emp) => emp.profession != "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const arr1 = [
+    { id: 5, name: "rose", age: "24", profession: "Software Tester" },
+    { id: 6, name: "modi", age: "26", profession: "Software Trainee" },
+    { id: 7, name: "Abdul", age: "30", profession: "Bug Hunter" },
+  ];
+  console.log(arr.concat(arr1));
 }
